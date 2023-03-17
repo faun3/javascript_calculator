4a0866daa6aaf03da909ba9bc5a0777cf7ed25ef
faun3 <antonioculcea@gmail.com>
2023-03-17 16:58:46 +0200

    write basic operation functions

---
+++ calc.js
@@ -0,0 +1,38 @@
+function operate(operator, operand1, operand2)
+{
+    switch (operator){
+        case "+":
+            return add(operand1, operand2);
+        case "-":
+            return subtract(operand1, operand2);
+        case "*":
+            return multiply(operand1, operand2);
+        case "/":
+            return divide(operand1, operand2);
+    }
+}
+
+function add(operand1, operand2)
+{
+    return (operand1 + operand2);
+}
+
+function subtract(operand1, operand2)
+{
+    return (operand1 - operand2);
+}
+
+function multiply(operand1, operand2)
+{
+    return (operand1 * operand2);
+}
+
+function divide(operand1, operand2)
+{
+    if (operand2 === 0){
+        alert("Division by 0 is impossible!");
+        console.log("Divide by 0!");
+        return;
+    }
+    return (operand1 / operand2);
+}
